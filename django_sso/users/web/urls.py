800a836bddf6bb51ff63fa75275defb76f337792
Dakshesh Jain <dakshesh.jain14@gmail.com>
2025-06-28 22:55:17 +0530

    feat: made views for password reset flow

--- django_sso/users/web/urls.py
+++ django_sso/users/web/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import AuthorizeView, LoginView, RegisterView
+from .views import (
+    AuthorizeView,
+    LoginView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+    RegisterView,
+)
 
 app_name = "accounts"
 
@@ -8,4 +16,9 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("authorize/", AuthorizeView.as_view(), name="authorize"),
+    # password reset flow
+    path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("password-reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
