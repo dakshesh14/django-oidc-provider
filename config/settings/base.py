81c7348690d43ed5a14b7b83834f68d8ef687872
Dakshesh Jain <dakshesh.jain14@gmail.com>
2025-07-13 20:09:26 +0530

    refactor: moved wsgi file location and delete asgi(unused)

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
 ROOT_URLCONF = "config.urls"
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
-WSGI_APPLICATION = "django_sso.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 # APPS
 # ------------------------------------------------------------------------------
