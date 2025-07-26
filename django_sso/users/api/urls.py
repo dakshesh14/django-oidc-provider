eb0d9cb8d8a6cb4d77fb1a6a64423fb72f2d2d61
Dakshesh Jain <dakshesh.jain14@gmail.com>
2025-07-26 20:19:23 +0530

    feat: added jwks endpoint

--- django_sso/users/api/urls.py
+++ django_sso/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import LogoutView, RefreshTokenView, TokenView, UserInfoView
+from .views import JWKSView, LogoutView, RefreshTokenView, TokenView, UserInfoView
 
 app_name = "accounts"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("token/refresh/", RefreshTokenView.as_view(), name="token"),
     path("userinfo/", UserInfoView.as_view(), name="userinfo"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("jwks/", JWKSView.as_view(), name="jwks"),
 ]
